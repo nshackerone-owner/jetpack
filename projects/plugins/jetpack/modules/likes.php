8bf668fdbe9f4229f1e71a12c7869e575ed33f98
Juan Adolfo Lucha García <juanlucha@gmail.com>
2023-12-01 17:02:56 +0100

    Removed feature flag for new likes layout

--- projects/plugins/jetpack/modules/likes.php
+++ projects/plugins/jetpack/modules/likes.php
@@ -432,19 +432,8 @@ class Jetpack_Likes {
 		* If the same post appears more then once on a page the page goes crazy
 		* we need a slightly more unique id / name for the widget wrapper.
 		*/
-		$uniqid = uniqid();
-		/**
-		 * Enable an alternate Likes layout.
-		 *
-		 * @since 12.9
-		 *
-		 * @module likes
-		 *
-		 * @param bool $new_layout Enable the new Likes layout. False by default.
-		 */
-		$new_layout = apply_filters( 'likes_new_layout', false ) ? '&amp;n=1' : '';
-
-		$src      = sprintf( 'https://widgets.wp.com/likes/#blog_id=%1$d&amp;post_id=%2$d&amp;origin=%3$s&amp;obj_id=%1$d-%2$d-%4$s%5$s', $blog_id, $post_id, $domain, $uniqid, $new_layout );
+		$uniqid   = uniqid();
+		$src      = sprintf( 'https://widgets.wp.com/likes/#blog_id=%1$d&amp;post_id=%2$d&amp;origin=%3$s&amp;obj_id=%1$d-%2$d-%4$s', $blog_id, $post_id, $domain, $uniqid );
 		$name     = sprintf( 'like-post-frame-%1$d-%2$d-%3$s', $blog_id, $post_id, $uniqid );
 		$wrapper  = sprintf( 'like-post-wrapper-%1$d-%2$d-%3$s', $blog_id, $post_id, $uniqid );
 		$headline = sprintf(
